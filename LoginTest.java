4aa4e17cb57d7cf1cf4623d6b12196365309e370
BSGopiKrishna <ggopi98102@gmail.com>
2026-04-15 16:49:50 +0530

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,43 @@
+package drem;
+
+
+
+import com.microsoft.playwright.*;
+
+public class LoginTest {
+    
+    	public static void main(String[]args) throws InterruptedException {
+			
+			Playwright playwright = Playwright.create();
+		      
+		      BrowserType browserType = playwright.chromium();
+		      
+		      Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chromium"));
+		      
+		      BrowserContext browserContext = browser.newContext();
+		      
+		      Page page = browserContext.newPage();
+		      
+		      page.navigate("https://www.saucedemo.com/");
+		      Thread.sleep(2000);
+		      
+		      page.fill("#user-name","problem_user");
+		    
+		      Thread.sleep(2000);
+		      page.fill("#password", "secret_sauce");
+		      Thread.sleep(2000);
+		      page.click("#login-button");
+		      Thread.sleep(2000);
+		      
+		      System.out.println(page.title());
+		      if(page.isVisible(".title")) {
+		    	  System.out.println("Login successfull, login page loaded : ");
+		      }else {
+		    	  System.out.println("Login faild :");
+		      }
+		    //  playwright.close();
+		      browser.close();
+		      
+    	}
+		    
+    }
